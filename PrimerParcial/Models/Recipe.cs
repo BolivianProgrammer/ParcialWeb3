10d7923b3eedfb70b1c4aca93354690687270914
BolivianProgrammer <tylerj@tylerlabs.com>
2025-09-25 19:28:59 -0400

    First commit with recipe model added

---
+++ PrimerParcial/Models/Recipe.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PrimerParcial.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public int PreparationTimeMinutes { get; set; }
+
+        [Required]
+        public int Servings { get; set; }
+
+        [Required]
+        public string Instructions { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        // Foreign Key for Category
+        public int CategoryId { get; set; }
+
+        // Navigation Properties
+        public Category Category { get; set; }
+        public ICollection<Ingredient> Ingredients { get; set; }
+    }
+}
