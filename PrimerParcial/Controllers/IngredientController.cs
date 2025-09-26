589245b1f53de90f4cde5bd29eaedd2e64fbba38
BolivianProgrammer <tylerj@tylerlabs.com>
2025-09-25 20:02:38 -0400

    Added Controllers for all Models

---
+++ PrimerParcial/Controllers/IngredientController.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PrimerParcial.Data;
+using PrimerParcial.Models;
+
+namespace PrimerParcial.Controllers
+{
+    public class IngredientController : Controller
+    {
+        private readonly RecetasDBContext _context;
+
+        public IngredientController(RecetasDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Ingredient
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Ingredients.Include(i => i.Recipe).ToListAsync());
+        }
+
+        // GET: Ingredient/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ingredient = await _context.Ingredients
+                .Include(i => i.Recipe)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ingredient == null)
+            {
+                return NotFound();
+            }
+
+            return View(ingredient);
+        }
+
+        // GET: Ingredient/Create
+        public IActionResult Create()
+        {
+            ViewData["RecipeId"] = new SelectList(_context.Recipes, "Id", "Title");
+            return View();
+        }
+
+        // POST: Ingredient/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Quantity,RecipeId")] Ingredient ingredient)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ingredient);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RecipeId"] = new SelectList(_context.Recipes, "Id", "Title", ingredient.RecipeId);
+            return View(ingredient);
+        }
+
+        // GET: Ingredient/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ingredient = await _context.Ingredients.FindAsync(id);
+            if (ingredient == null)
+            {
+                return NotFound();
+            }
+            ViewData["RecipeId"] = new SelectList(_context.Recipes, "Id", "Title", ingredient.RecipeId);
+            return View(ingredient);
+        }
+
+        // POST: Ingredient/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Quantity,RecipeId")] Ingredient ingredient)
+        {
+            if (id != ingredient.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ingredient);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!IngredientExists(ingredient.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RecipeId"] = new SelectList(_context.Recipes, "Id", "Title", ingredient.RecipeId);
+            return View(ingredient);
+        }
+
+        // GET: Ingredient/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ingredient = await _context.Ingredients
+                .Include(i => i.Recipe)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ingredient == null)
+            {
+                return NotFound();
+            }
+
+            return View(ingredient);
+        }
+
+        // POST: Ingredient/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var ingredient = await _context.Ingredients.FindAsync(id);
+            if (ingredient != null)
+            {
+                _context.Ingredients.Remove(ingredient);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool IngredientExists(int id)
+        {
+            return _context.Ingredients.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ PrimerParcial/Controllers/RecipeController.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PrimerParcial.Data;
+using PrimerParcial.Models;
+
+namespace PrimerParcial.Controllers
+{
+    public class RecipeController : Controller
+    {
+        private readonly RecetasDBContext _context;
+
+        public RecipeController(RecetasDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Recipe
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Recipes.Include(r => r.Category).ToListAsync());
+        }
+
+        // GET: Recipe/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var recipe = await _context.Recipes
+                .Include(r => r.Category)
+                .Include(r => r.Ingredients)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            return View(recipe);
+        }
+
+        // GET: Recipe/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name");
+            return View();
+        }
+
+        // POST: Recipe/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,PreparationTimeMinutes,Servings,Instructions,CategoryId")] Recipe recipe)
+        {
+            if (ModelState.IsValid)
+            {
+                recipe.DateCreated = DateTime.Now;
+                _context.Add(recipe);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", recipe.CategoryId);
+            return View(recipe);
+        }
+
+        // GET: Recipe/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var recipe = await _context.Recipes.FindAsync(id);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", recipe.CategoryId);
+            return View(recipe);
+        }
+
+        // POST: Recipe/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,PreparationTimeMinutes,Servings,Instructions,CategoryId,DateCreated")] Recipe recipe)
+        {
+            if (id != recipe.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(recipe);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RecipeExists(recipe.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", recipe.CategoryId);
+            return View(recipe);
+        }
+
+        // GET: Recipe/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var recipe = await _context.Recipes
+                .Include(r => r.Category)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            return View(recipe);
+        }
+
+        // POST: Recipe/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var recipe = await _context.Recipes.FindAsync(id);
+            if (recipe != null)
+            {
+                _context.Recipes.Remove(recipe);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RecipeExists(int id)
+        {
+            return _context.Recipes.Any(e => e.Id == id);
+        }
+    }
+}
