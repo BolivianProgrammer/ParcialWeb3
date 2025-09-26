137abc4f2557503680475eb6e97138c328e3c422
BolivianProgrammer <tylerj@tylerlabs.com>
2025-09-25 20:32:40 -0400

    Added Nullable check

--- PrimerParcial/Models/Recipe.cs
+++ PrimerParcial/Models/Recipe.cs
@@ -27,7 +27,7 @@ namespace PrimerParcial.Models
 
         public int CategoryId { get; set; }
 
-        public Category Category { get; set; } = null!;
+        public Category? Category { get; set; } = null!;
         public ICollection<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
     }
 }
